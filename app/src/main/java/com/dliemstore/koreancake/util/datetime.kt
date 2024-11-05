3e24077f2670c8fa9b393cc15e1158a3e37f563d
Adhicitta Masran <adhicittamasran@gmail.com>
2024-11-05 21:55:52 +0800

    add datetime utilities

---
+++ app/src/main/java/com/dliemstore/koreancake/util/datetime.kt
@@ -0,0 +1,48 @@
+package com.dliemstore.koreancake.util
+
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.Period
+import java.time.ZoneId
+import java.util.Date
+import java.util.Locale
+
+fun Long.epochToDate(): Date {
+    return Date(this)
+}
+
+fun Date.formatDate(): String {
+    val date = SimpleDateFormat("EEE, d MMMM yyyy", Locale.getDefault()).format(this)
+    val dayName = date.split(",")[0]
+
+    return date.replace(dayName, dayName.translateDayToIndonesia())
+}
+
+fun String.translateDayToIndonesia(): String {
+    val days = mapOf(
+        "Sun" to "Minggu",
+        "Mon" to "Senin",
+        "Tue" to "Selasa",
+        "Wed" to "Rabu",
+        "Thu" to "Kamis",
+        "Fri" to "Jumat",
+        "Sat" to "Sabtu"
+    )
+
+    return days.getValue(this)
+}
+
+fun Date.formatTime(): String {
+    return SimpleDateFormat("HH:mm", Locale.getDefault()).format(this)
+}
+
+fun Date.countRemainingDay(): Int {
+    val today = LocalDate.now()
+
+//    convert input date to LocalDate
+    val instant = this.toInstant()
+    val zonedDateTime = instant.atZone(ZoneId.systemDefault())
+    val to = zonedDateTime.toLocalDate()
+
+    return Period.between(today, to).days
+}
