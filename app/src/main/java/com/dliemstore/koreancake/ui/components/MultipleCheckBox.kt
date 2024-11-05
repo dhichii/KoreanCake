efd62666709b5c83b909a96bdd6eee0f7aa2f0a0
Adhicitta Masran <adhicittamasran@gmail.com>
2024-11-05 21:58:02 +0800

    add multiple checkbox component

---
+++ app/src/main/java/com/dliemstore/koreancake/ui/components/MultipleCheckBox.kt
@@ -0,0 +1,60 @@
+package com.dliemstore.koreancake.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.dliemstore.koreancake.util.Progress
+
+@Composable
+fun MultipleCheckBox(data: List<Progress>) {
+    var checkState by remember {
+        mutableStateOf(listOf<Int>())
+    }
+
+    Column {
+        data.forEach { item ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(32.dp)
+                    .selectable(
+                        selected = checkState.contains(item.id),
+                        onClick = {
+                            checkState = if (checkState.contains(item.id)) {
+                                checkState - item.id
+                            } else {
+                                checkState + item.id
+                            }
+                        },
+                        role = Role.Checkbox
+                    )
+            ) {
+                Checkbox(
+                    checked = checkState.contains(item.id),
+                    onCheckedChange = null,
+                    modifier = Modifier.padding(end = 8.dp)
+                )
+                Text(
+                    text = item.name,
+                    fontSize = 12.sp
+                )
+            }
+        }
+    }
+}
