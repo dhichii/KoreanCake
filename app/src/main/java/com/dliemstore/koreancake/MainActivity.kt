62d87f61ad0e150577dd9a1730d22515211a8616
Adhicitta Masran <adhicittamasran@gmail.com>
2024-11-19 13:28:31 +0800

    feat(splashscreen): add splashscreen

--- app/src/main/java/com/dliemstore/koreancake/MainActivity.kt
+++ app/src/main/java/com/dliemstore/koreancake/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.dliemstore.koreancake.ui.navigation.graphs.RootNavigationGraph
 import com.dliemstore.koreancake.ui.theme.KoreanCakeTheme
@@ -11,6 +12,7 @@ import com.dliemstore.koreancake.ui.theme.KoreanCakeTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             KoreanCakeTheme {
