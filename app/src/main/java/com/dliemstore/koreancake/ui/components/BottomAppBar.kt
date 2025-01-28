1603e26650fc4ca17872ded7cb64ebfc21dd1419
Adhicitta Masran <adhicittamasran@gmail.com>
2025-01-28 20:06:15 +0800

    feat: reshape SaveBottomAppBar to medium

--- app/src/main/java/com/dliemstore/koreancake/ui/components/BottomAppBar.kt
+++ app/src/main/java/com/dliemstore/koreancake/ui/components/BottomAppBar.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
@@ -101,6 +102,7 @@ fun SaveBottomAppBar(onClick: () -> Unit) {
     BottomAppBar {
         Button(
             onClick = onClick,
+            shape = MaterialTheme.shapes.medium,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(start = 12.dp, end = 12.dp)
