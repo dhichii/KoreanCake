ab9da955ea6fe456ccc949a221d75598fe5d7d96
Adhicitta Masran <adhicittamasran@gmail.com>
2025-02-12 11:41:48 +0800

    feat(process): add main screen

--- app/src/main/java/com/dliemstore/koreancake/ui/navigation/graphs/ProcessNavigationGraph.kt
+++ app/src/main/java/com/dliemstore/koreancake/ui/navigation/graphs/ProcessNavigationGraph.kt
@@ -2,16 +2,23 @@ package com.dliemstore.koreancake.ui.navigation.graphs
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Reorder
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import com.dliemstore.koreancake.ui.components.BottomNavigationBar
+import com.dliemstore.koreancake.ui.components.SaveBottomAppBar
+import com.dliemstore.koreancake.ui.screens.process.Process
 
 enum class ProcessScreen {
     PROCESS_MAIN,
@@ -34,8 +41,10 @@ fun NavGraphBuilder.processNavigationGraph(
         startDestination = ProcessNavigationItem.Main.route,
     ) {
         composable(route = ProcessNavigationItem.Main.route) {
+            var isReorderEnabled by remember { mutableStateOf(false) }
+
             scaffoldViewState.value = ScaffoldViewState(
-                topAppBar = TopAppBarItem(
+                topAppBar = if (!isReorderEnabled) TopAppBarItem(
                     title = { Text("Process") },
                     actions = {
                         IconButton(onClick = {
@@ -46,16 +55,31 @@ fun NavGraphBuilder.processNavigationGraph(
                                 contentDescription = "Add"
                             )
                         }
-                        IconButton(onClick = {}) {
+                        IconButton(onClick = { isReorderEnabled = true }) {
                             Icon(
                                 imageVector = Icons.Filled.Reorder,
                                 contentDescription = "Reorder"
                             )
                         }
                     }
+                ) else TopAppBarItem(
+                    title = { Text("Process") },
+                    navigationIcon = TopAppBarNavigationIcon.Custom(
+                        icon = Icons.Filled.Close,
+                        contentDescription = "Close",
+                        onClick = { isReorderEnabled = false })
                 ),
-                bottomAppBar = { BottomNavigationBar(navController) }
+                bottomAppBar = {
+                    if (!isReorderEnabled) {
+                        BottomNavigationBar(
+                            navController
+                        )
+                    } else {
+                        SaveBottomAppBar(onClick = { isReorderEnabled = false })
+                    }
+                }
             )
+            Process(navController, isReorderEnabled)
         }
 
         composable(route = ProcessNavigationItem.Add.route) {
---
+++ app/src/main/java/com/dliemstore/koreancake/ui/screens/process/Process.kt
@@ -0,0 +1,127 @@
+package com.dliemstore.koreancake.ui.screens.process
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Reorder
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.dliemstore.koreancake.ui.components.DeleteDialog
+import com.dliemstore.koreancake.ui.navigation.graphs.ProcessNavigationItem
+import com.dliemstore.koreancake.util.ProcessData
+import com.dliemstore.koreancake.util.getAllProcesses
+import sh.calvin.reorderable.ReorderableCollectionItemScope
+import sh.calvin.reorderable.ReorderableItem
+import sh.calvin.reorderable.rememberReorderableLazyListState
+
+@Composable
+fun Process(navController: NavController, isReorderEnabled: Boolean) {
+    var data by remember { mutableStateOf(getAllProcesses()) }
+    val lazyListState = rememberLazyListState()
+    val reorderableLazyColumnState = rememberReorderableLazyListState(lazyListState) { from, to ->
+        data = data.toMutableList().apply {
+            add(to.index, removeAt(from.index))
+            forEachIndexed { index, item ->
+                set(index, item.copy(step = index + 1))
+            }
+        }
+    }
+
+    var isShowDeleteDialog by remember { mutableStateOf(false) }
+
+    LazyColumn(state = lazyListState, modifier = Modifier.fillMaxSize()) {
+        items(data, key = { it.id }) { item ->
+            ReorderableItem(reorderableLazyColumnState, key = item.id) {
+                ProcessListItem(
+                    item = item,
+                    isReorderEnabled = isReorderEnabled,
+                    onEdit = { navController.navigate("${ProcessNavigationItem.Edit.route}/${item.id}") },
+                    onDelete = {
+                        isShowDeleteDialog = true
+                    },
+                    reorderScope = this
+                )
+            }
+        }
+    }
+
+    if (isShowDeleteDialog) {
+        DeleteDialog(
+            onDismiss = { isShowDeleteDialog = false },
+            onConfirmation = {
+                isShowDeleteDialog = false
+            }
+        )
+    }
+}
+
+@Composable
+fun ProcessListItem(
+    item: ProcessData,
+    isReorderEnabled: Boolean,
+    onEdit: () -> Unit,
+    onDelete: () -> Unit,
+    reorderScope: ReorderableCollectionItemScope,
+) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceContainerLowest
+        ),
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+        modifier = Modifier
+            .padding(12.dp, 4.dp)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Row(modifier = Modifier.padding(12.dp, 8.dp)) {
+                Text("${item.step}.")
+                Spacer(Modifier.width(8.dp))
+                Text(item.name)
+            }
+
+            Row {
+                if (!isReorderEnabled) {
+                    IconButton(onClick = onEdit) {
+                        Icon(imageVector = Icons.Filled.Edit, contentDescription = "Edit")
+                    }
+                    Spacer(Modifier.width(4.dp))
+                    IconButton(onClick = onDelete) {
+                        Icon(imageVector = Icons.Filled.Delete, contentDescription = "Delete")
+                    }
+                } else {
+                    IconButton(
+                        onClick = {},
+                        modifier = with(reorderScope) { Modifier.draggableHandle() }) {
+                        Icon(imageVector = Icons.Filled.Reorder, contentDescription = "Reorder")
+                    }
+                }
+            }
+        }
+    }
+}
