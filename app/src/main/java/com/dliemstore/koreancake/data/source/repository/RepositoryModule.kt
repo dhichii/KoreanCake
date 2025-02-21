b8762ac6546711d6c45781f5e40ff94fddb52d86
Adhicitta Masran <adhicittamasran@gmail.com>
2025-02-21 21:49:08 +0800

    feat(auth): add auth repository

---
+++ app/src/main/java/com/dliemstore/koreancake/data/source/repository/RepositoryModule.kt
@@ -0,0 +1,19 @@
+package com.dliemstore.koreancake.data.source.repository
+
+import com.dliemstore.koreancake.data.api.service.AuthService
+import com.dliemstore.koreancake.data.source.repository.auth.AuthRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+    @Provides
+    @Singleton
+    fun provideAuthRepository(authService: AuthService): AuthRepository {
+        return AuthRepository(authService)
+    }
+}
---
+++ app/src/main/java/com/dliemstore/koreancake/data/source/repository/auth/AuthRepository.kt
@@ -0,0 +1,7 @@
+package com.dliemstore.koreancake.data.source.repository.auth
+
+import com.dliemstore.koreancake.data.api.service.AuthService
+import javax.inject.Inject
+
+class AuthRepository @Inject constructor(private val authService: AuthService) {
+}
