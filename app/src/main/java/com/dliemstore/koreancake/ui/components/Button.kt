55282977b6ec130518839ef01b2833a1766dba3f
Adhicitta Masran <adhicittamasran@gmail.com>
2025-02-23 19:55:45 +0800

    feat: add circular loading to button components

--- app/src/main/java/com/dliemstore/koreancake/ui/components/Button.kt
+++ app/src/main/java/com/dliemstore/koreancake/ui/components/Button.kt
@@ -1,15 +1,19 @@
 package com.dliemstore.koreancake.ui.components
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun PrimaryButton(
     text: String,
+    isLoading: Boolean = false,
     enabled: Boolean = true,
     onClick: () -> Unit,
     modifier: Modifier = Modifier
@@ -17,16 +21,24 @@ fun PrimaryButton(
     Button(
         onClick = { onClick() },
         shape = MaterialTheme.shapes.medium,
-        enabled = enabled,
+        enabled = !isLoading && enabled,
         modifier = modifier
     ) {
-        Text(text)
+        if (!isLoading) {
+            Text(text)
+        } else {
+            CircularProgressIndicator(
+                strokeWidth = 2.dp,
+                modifier = Modifier.size(20.dp)
+            )
+        }
     }
 }
 
 @Composable
 fun SecondaryButton(
     text: String,
+    isLoading: Boolean = false,
     enabled: Boolean = true,
     onClick: () -> Unit,
     modifier: Modifier = Modifier
@@ -34,9 +46,16 @@ fun SecondaryButton(
     OutlinedButton(
         onClick = { onClick() },
         shape = MaterialTheme.shapes.medium,
-        enabled = enabled,
+        enabled = !isLoading && enabled,
         modifier = modifier
     ) {
-        Text(text)
+        if (!isLoading) {
+            Text(text)
+        } else {
+            CircularProgressIndicator(
+                strokeWidth = 2.dp,
+                modifier = Modifier.size(20.dp)
+            )
+        }
     }
 }
