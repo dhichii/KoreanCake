334eec19f85cedd7f99ccf28571f7b515bee66b1
Adhicitta Masran <adhicittamasran@gmail.com>
2025-02-23 21:38:45 +0800

    feat(register): integrate the UI with viewmodel

--- app/src/main/java/com/dliemstore/koreancake/ui/screens/register/Register.kt
+++ app/src/main/java/com/dliemstore/koreancake/ui/screens/register/Register.kt
@@ -1,51 +1,55 @@
 package com.dliemstore.koreancake.ui.screens.register
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.dliemstore.koreancake.ui.components.PasswordInput
 import com.dliemstore.koreancake.ui.components.PrimaryButton
 import com.dliemstore.koreancake.ui.components.SecondaryButton
 import com.dliemstore.koreancake.ui.components.TextInput
 import com.dliemstore.koreancake.ui.navigation.graphs.AuthNavigationItem
+import com.dliemstore.koreancake.ui.viewmodel.auth.RegisterViewModel
 
 @Composable
-fun Register(navController: NavController) {
-    var name by rememberSaveable { mutableStateOf("") }
-    var username by rememberSaveable { mutableStateOf("") }
-    var email by rememberSaveable { mutableStateOf("") }
-    var password by rememberSaveable { mutableStateOf("") }
-    var confirmPassword by rememberSaveable { mutableStateOf("") }
+fun Register(navController: NavController, viewModel: RegisterViewModel = hiltViewModel()) {
+    val registerState by viewModel.registerState.collectAsState()
+    val context = LocalContext.current
+    val isPrimaryButtonEnabled =
+        registerState.name.isNotBlank() && registerState.email.isNotBlank() &&
+                registerState.username.isNotBlank() && registerState.password.isNotBlank() &&
+                registerState.confirmPassword.isNotBlank()
 
-    val hasMinimum: Boolean = password.matches(Regex(".{8,}"))
-    val isMatched: Boolean = confirmPassword == password
-    var isConfirmPasswordChanged by rememberSaveable { mutableStateOf(false) }
+    LaunchedEffect(registerState.isSuccess) {
+        if (registerState.isSuccess) {
+            Toast.makeText(context, "Registrasi berhasil!", Toast.LENGTH_SHORT).show()
+            navController.navigate(AuthNavigationItem.Login.route)
+        }
+    }
+
+    LaunchedEffect(registerState.errorMessage) {
+        registerState.errorMessage?.let {
+            Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+        }
+    }
 
     Surface {
         Box(
@@ -57,51 +61,43 @@ fun Register(navController: NavController) {
             Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
                 Text(text = "Buat Akun", fontSize = 24.sp, fontWeight = FontWeight.Bold)
                 TextInput(
-                    value = name,
-                    onInputChanged = { name = it },
-                    label = "Nama"
+                    value = registerState.name,
+                    onInputChanged = { viewModel.onInputChange("name", it) },
+                    label = "Nama",
+                    errorMessage = registerState.errors["name"]
                 )
                 TextInput(
-                    value = username,
-                    onInputChanged = { username = it },
-                    label = "Username"
+                    value = registerState.username,
+                    onInputChanged = { viewModel.onInputChange("username", it) },
+                    label = "Username",
+                    errorMessage = registerState.errors["username"]
                 )
                 TextInput(
-                    value = email,
-                    onInputChanged = { email = it },
+                    value = registerState.email,
+                    onInputChanged = { viewModel.onInputChange("email", it) },
                     label = "Email",
-                    keyboardType = KeyboardType.Email
+                    keyboardType = KeyboardType.Email,
+                    errorMessage = registerState.errors["email"]
                 )
-                Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
-                    PasswordInput(value = password, onInputChanged = { password = it })
-                    Row {
-                        Icon(
-                            imageVector = if (hasMinimum) Icons.Filled.Check else Icons.Filled.Close,
-                            tint = if (hasMinimum) Color.Green else MaterialTheme.colorScheme.error,
-                            contentDescription = "has minimum"
-                        )
-                        Spacer(modifier = Modifier.width(4.dp))
-                        Text(
-                            text = "Minimal 8 huruf",
-                            color = MaterialTheme.colorScheme.inverseSurface
-                        )
-                    }
-                }
-                Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
-                    PasswordInput(
-                        value = confirmPassword,
-                        label = "Konfirmasi Password",
-                        onInputChanged = {
-                            confirmPassword = it
-                            if (!isConfirmPasswordChanged) isConfirmPasswordChanged = true
-                        },
-                        isError = !isMatched && isConfirmPasswordChanged
-                    )
-                    if (!isMatched && isConfirmPasswordChanged) Text("Konfirmasi password tidak sama")
-                }
+                PasswordInput(
+                    value = registerState.password,
+                    onInputChanged = { viewModel.onInputChange("password", it) },
+                    errorMessage = registerState.errors["password"]
+                )
+                PasswordInput(
+                    value = registerState.confirmPassword,
+                    label = "Konfirmasi Password",
+                    onInputChanged = {
+                        viewModel.onInputChange("confirmPassword", it)
+                    },
+                    errorMessage = registerState.errors["confirmPassword"]
+                )
+
                 PrimaryButton(
                     text = "Daftar",
-                    onClick = { navController.navigate(AuthNavigationItem.Login.route) },
+                    isLoading = registerState.isLoading,
+                    enabled = isPrimaryButtonEnabled,
+                    onClick = { viewModel.register() },
                     modifier = Modifier.fillMaxWidth()
                 )
             }
