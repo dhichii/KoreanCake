ff4d403facc6147b43b68648b84deb3389102780
Adhicitta Masran <adhicittamasran@gmail.com>
2025-02-23 11:48:08 +0800

    feat(auth): add RegisterState

---
+++ app/src/main/java/com/dliemstore/koreancake/ui/state/RegisterState.kt
@@ -0,0 +1,13 @@
+package com.dliemstore.koreancake.ui.state
+
+data class RegisterState(
+    val name: String = "",
+    val username: String = "",
+    val email: String = "",
+    val password: String = "",
+    val confirmPassword: String = "",
+    val errors: Map<String, String?> = emptyMap(),
+    val isLoading: Boolean = false,
+    val isSuccess: Boolean = false,
+    val errorMessage: String? = null
+)
