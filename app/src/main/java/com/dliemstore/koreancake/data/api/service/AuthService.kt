7636b51a066cd3802ce0b787bacc778c3a03415f
Adhicitta Masran <adhicittamasran@gmail.com>
2025-02-25 00:29:42 +0800

    feat(auth): add login service

--- app/src/main/java/com/dliemstore/koreancake/data/api/service/AuthService.kt
+++ app/src/main/java/com/dliemstore/koreancake/data/api/service/AuthService.kt
@@ -1,6 +1,9 @@
 package com.dliemstore.koreancake.data.api.service
 
+import com.dliemstore.koreancake.data.source.remote.request.auth.LoginRequest
 import com.dliemstore.koreancake.data.source.remote.request.auth.RegisterRequest
+import com.dliemstore.koreancake.data.source.remote.response.SuccessResponse
+import com.dliemstore.koreancake.data.source.remote.response.auth.TokenResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -8,4 +11,7 @@ import retrofit2.http.POST
 interface AuthService {
     @POST("auth/register")
     suspend fun register(@Body registerRequest: RegisterRequest): Response<Unit>
+
+    @POST("auth/login")
+    suspend fun login(@Body loginRequest: LoginRequest): Response<SuccessResponse<TokenResponse>>
 }
