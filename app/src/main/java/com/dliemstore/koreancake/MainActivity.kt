313ba74977ee811f1a0b8aedb721b38097ba02ca
Adhicitta Masran <adhicittamasran@gmail.com>
2025-02-26 20:41:45 +0800

    refactor: use token manager to check if the user is logged in

--- app/src/main/java/com/dliemstore/koreancake/MainActivity.kt
+++ app/src/main/java/com/dliemstore/koreancake/MainActivity.kt
@@ -6,19 +6,24 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
+import com.dliemstore.koreancake.data.api.TokenManager
 import com.dliemstore.koreancake.ui.navigation.graphs.RootNavigationGraph
 import com.dliemstore.koreancake.ui.theme.KoreanCakeTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @Inject
+    lateinit var tokenManager: TokenManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         enableEdgeToEdge()
         setContent {
             val navController = rememberNavController()
-            val isLoggedIn = false
+            val isLoggedIn = tokenManager.getToken() != null
 
             KoreanCakeTheme {
                 RootNavigationGraph(isLoggedIn, navController)
