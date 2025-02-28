3b52256723487da2a9135a96eb0cf0c8907dfece
Adhicitta Masran <adhicittamasran@gmail.com>
2025-03-01 00:13:45 +0800

    feat(order): add API endpoint for fetching orders

--- app/src/main/java/com/dliemstore/koreancake/data/api/service/OrderService.kt
+++ app/src/main/java/com/dliemstore/koreancake/data/api/service/OrderService.kt
@@ -1,4 +1,16 @@
 package com.dliemstore.koreancake.data.api.service
 
+import com.dliemstore.koreancake.data.source.remote.response.PaginationSuccessResponse
+import com.dliemstore.koreancake.data.source.remote.response.order.OrdersResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface OrderService {
+    @GET("orders")
+    suspend fun getAll(
+        @Query("page") page: Int,
+        @Query("limit") limit: Int,
+        @Query("status") status: String
+    ): Response<PaginationSuccessResponse<OrdersResponse>>
 }
