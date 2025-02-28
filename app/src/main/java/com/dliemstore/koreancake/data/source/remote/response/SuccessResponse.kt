e92578fbdee80fb0f1368e981dcd1b84d2c5069a
Adhicitta Masran <adhicittamasran@gmail.com>
2025-02-28 18:50:36 +0800

    feat: add PaginationSuccessResponse data class

--- app/src/main/java/com/dliemstore/koreancake/data/source/remote/response/SuccessResponse.kt
+++ app/src/main/java/com/dliemstore/koreancake/data/source/remote/response/SuccessResponse.kt
@@ -5,3 +5,11 @@ import com.google.gson.annotations.SerializedName
 data class SuccessResponse<T>(
     @SerializedName("data") val data: T,
 )
+
+data class PaginationSuccessResponse<T>(
+    @SerializedName("limit") val limit: Int,
+    @SerializedName("totalPage") val totalPage: Int,
+    @SerializedName("totalResult") val totalResult: Int,
+    @SerializedName("page") val page: Int,
+    @SerializedName("data") val data: List<T>,
+)
