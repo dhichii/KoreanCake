ee6e1e29c550475bfa2345019e705d1d9124c61c
Adhicitta Masran <adhicittamasran@gmail.com>
2025-03-05 22:09:24 +0800

    feat(order): add getOrderDetail and updateOrderProgress repository function

--- app/src/main/java/com/dliemstore/koreancake/data/source/repository/order/OrderRepository.kt
+++ app/src/main/java/com/dliemstore/koreancake/data/source/repository/order/OrderRepository.kt
@@ -5,10 +5,15 @@ import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.dliemstore.koreancake.data.api.service.OrderService
 import com.dliemstore.koreancake.data.paging.BasePagingSource
+import com.dliemstore.koreancake.data.source.remote.request.order.UpdateOrderProgressRequest
 import com.dliemstore.koreancake.data.source.remote.response.order.OrdersResponse
+import com.dliemstore.koreancake.util.ApiUtils
+import com.dliemstore.koreancake.util.Resource
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
+import java.io.IOException
 import javax.inject.Inject
 
 class OrderRepository @Inject constructor(
@@ -29,4 +34,62 @@ class OrderRepository @Inject constructor(
             }
         ).flow.flowOn(Dispatchers.IO)
     }
+
+    fun getOrderDetail(id: String) = flow {
+        emit(Resource.Loading())
+
+        try {
+            val response =
+                orderService.getById(id)
+            if (response.isSuccessful) {
+                emit(Resource.Success(response.body()?.data, response.code()))
+            } else {
+                val errorResponse = runCatching { ApiUtils.parseError(response) }.getOrNull()
+                val errorMessage = when (response.code()) {
+                    500 -> "Terjadi kesalahan pada server"
+                    else -> errorResponse?.message ?: "Permintaan tidak valid."
+                }
+
+                emit(Resource.Error(errorMessage, response.code(), errorResponse?.errors))
+            }
+        } catch (e: Exception) {
+            val errorMessage = if (e is IOException) {
+                "Tidak ada koneksi internet."
+            } else {
+                "Terjadi kesalahan yang tidak terduga"
+            }
+            emit(Resource.Error(errorMessage))
+        }
+    }.flowOn(Dispatchers.IO)
+
+    fun updateOrderProgress(orderId: String, progressId: String, isFinish: Boolean) = flow {
+        emit(Resource.Loading())
+
+        try {
+            val response =
+                orderService.updateOrderProgress(
+                    orderId,
+                    progressId,
+                    UpdateOrderProgressRequest(isFinish)
+                )
+            if (response.isSuccessful) {
+                emit(Resource.Success(Unit, response.code()))
+            } else {
+                val errorResponse = runCatching { ApiUtils.parseError(response) }.getOrNull()
+                val errorMessage = when (response.code()) {
+                    500 -> "Terjadi kesalahan pada server"
+                    else -> errorResponse?.message ?: "Permintaan tidak valid."
+                }
+
+                emit(Resource.Error(errorMessage, response.code(), errorResponse?.errors))
+            }
+        } catch (e: Exception) {
+            val errorMessage = if (e is IOException) {
+                "Tidak ada koneksi internet."
+            } else {
+                "Terjadi kesalahan yang tidak terduga"
+            }
+            emit(Resource.Error(errorMessage))
+        }
+    }.flowOn(Dispatchers.IO)
 }
