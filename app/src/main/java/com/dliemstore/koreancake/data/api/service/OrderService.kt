768766dfca38486fa2243a71f9f79af59f7a4788
Adhicitta Masran <adhicittamasran@gmail.com>
2025-03-05 22:05:45 +0800

    feat(order): add API endpoint to fetch order detail and update order progress

--- app/src/main/java/com/dliemstore/koreancake/data/api/service/OrderService.kt
+++ app/src/main/java/com/dliemstore/koreancake/data/api/service/OrderService.kt
@@ -1,9 +1,15 @@
 package com.dliemstore.koreancake.data.api.service
 
+import com.dliemstore.koreancake.data.source.remote.request.order.UpdateOrderProgressRequest
 import com.dliemstore.koreancake.data.source.remote.response.PaginationSuccessResponse
+import com.dliemstore.koreancake.data.source.remote.response.SuccessResponse
+import com.dliemstore.koreancake.data.source.remote.response.order.OrderDetailResponse
 import com.dliemstore.koreancake.data.source.remote.response.order.OrdersResponse
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PUT
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface OrderService {
@@ -13,4 +19,14 @@ interface OrderService {
         @Query("limit") limit: Int,
         @Query("status") status: String
     ): Response<PaginationSuccessResponse<OrdersResponse>>
+
+    @GET("orders/{id}")
+    suspend fun getById(@Path("id") id: String): Response<SuccessResponse<OrderDetailResponse>>
+
+    @PUT("orders/{id}/progresses/{progressId}")
+    suspend fun updateOrderProgress(
+        @Path("id") id: String,
+        @Path("progressId") progressId: String,
+        @Body request: UpdateOrderProgressRequest
+    ): Response<Unit>
 }
