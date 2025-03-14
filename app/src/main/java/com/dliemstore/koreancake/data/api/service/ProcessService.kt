b3131ffb4dc18bccce0b9f249ad1ccadeb5c12b5
Adhicitta Masran <adhicittamasran@gmail.com>
2025-03-14 13:35:54 +0800

    feat(process): add API endpoint add API endpoint to fetch processes, delete process, and update processes step

--- app/src/main/java/com/dliemstore/koreancake/data/api/service/ProcessService.kt
+++ app/src/main/java/com/dliemstore/koreancake/data/api/service/ProcessService.kt
@@ -1,4 +1,22 @@
 package com.dliemstore.koreancake.data.api.service
 
+import com.dliemstore.koreancake.data.source.remote.request.process.UpdateProcessesStepRequest
+import com.dliemstore.koreancake.data.source.remote.response.SuccessResponse
+import com.dliemstore.koreancake.data.source.remote.response.process.ProcessResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.PATCH
+import retrofit2.http.Path
+
 interface ProcessService {
+    @GET("processes")
+    suspend fun getAll(): Response<SuccessResponse<List<ProcessResponse>>>
+
+    @DELETE("processes/{id}")
+    suspend fun delete(@Path("id") id: String): Response<Unit>
+
+    @PATCH("processes/steps")
+    suspend fun updateProcessesStep(@Body request: List<UpdateProcessesStepRequest>): Response<Unit>
 }
