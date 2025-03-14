1b5da3547bda667d6d7d0a01ab6377e72cc84f23
Adhicitta Masran <adhicittamasran@gmail.com>
2025-03-14 13:34:10 +0800

    feat(process): add ProcessResponse and UpdateProcessesStepRequest

---
+++ app/src/main/java/com/dliemstore/koreancake/data/source/remote/request/process/UpdateProcessesStepRequest.kt
@@ -0,0 +1,6 @@
+package com.dliemstore.koreancake.data.source.remote.request.process
+
+data class UpdateProcessesStepRequest(
+    val id: String,
+    val step: Int
+)
---
+++ app/src/main/java/com/dliemstore/koreancake/data/source/remote/response/process/ProcessResponse.kt
@@ -0,0 +1,7 @@
+package com.dliemstore.koreancake.data.source.remote.response.process
+
+data class ProcessResponse(
+    val id: String,
+    val name: String,
+    val step: Int
+)
