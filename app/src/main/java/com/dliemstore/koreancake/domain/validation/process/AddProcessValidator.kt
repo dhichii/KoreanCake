c2fa4ca1691317160fad18453278476f38d35958
Adhicitta Masran <adhicittamasran@gmail.com>
2025-03-17 17:13:27 +0800

    feat(process): add adding process validator

---
+++ app/src/main/java/com/dliemstore/koreancake/domain/validation/process/AddProcessValidator.kt
@@ -0,0 +1,19 @@
+package com.dliemstore.koreancake.domain.validation.process
+
+import com.dliemstore.koreancake.util.ValidationHelper
+
+object AddProcessValidator {
+    fun validate(
+        name: String,
+        step: String
+    ): Map<String, String?> {
+        return mapOf(
+            "name" to listOfNotNull(ValidationHelper.required(name, "Nama")).firstOrNull(),
+            "step" to listOfNotNull(
+                ValidationHelper.required(step, "Langkah Proses"),
+                ValidationHelper.isValidInteger(step, "Langkah Proses"),
+                ValidationHelper.minInteger(step, 1, "Langkah Proses"),
+            ).firstOrNull()
+        ).filterValues { it != null }
+    }
+}
