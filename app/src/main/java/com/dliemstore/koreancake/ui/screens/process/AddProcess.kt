460e33b28666eb57db5951aedadb8cf85551a946
Adhicitta Masran <adhicittamasran@gmail.com>
2025-03-17 18:06:18 +0800

    feat(process): integrate AddProcess UI with viewmodel - handle states when success and error - handle error message in each input field - handle loading state in the save button - save button will be enabled when every input field is filled

--- app/src/main/java/com/dliemstore/koreancake/ui/screens/process/AddProcess.kt
+++ app/src/main/java/com/dliemstore/koreancake/ui/screens/process/AddProcess.kt
@@ -1,5 +1,6 @@
 package com.dliemstore.koreancake.ui.screens.process
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -11,23 +12,36 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.dliemstore.koreancake.ui.components.PrimaryButton
 import com.dliemstore.koreancake.ui.components.TextInput
 import com.dliemstore.koreancake.ui.navigation.graphs.ScaffoldViewState
 import com.dliemstore.koreancake.ui.navigation.graphs.TopAppBarItem
 import com.dliemstore.koreancake.ui.navigation.graphs.TopAppBarNavigationIcon
+import com.dliemstore.koreancake.ui.viewmodel.process.AddProcessViewModel
 
 @Composable
-fun AddProcess(navController: NavController, scaffoldViewState: MutableState<ScaffoldViewState>) {
-    var name by rememberSaveable { mutableStateOf("") }
-    var step by rememberSaveable { mutableStateOf("") }
+fun AddProcess(
+    navController: NavController,
+    scaffoldViewState: MutableState<ScaffoldViewState>,
+    viewModel: AddProcessViewModel = hiltViewModel()
+) {
+    val context = LocalContext.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    val addProcessState by viewModel.addProcessState.collectAsState()
+
+    val isPrimaryButtonEnabled =
+        addProcessState.name.isNotBlank() && addProcessState.step.isNotBlank()
+    val isAdding = addProcessState.isLoading
 
     LaunchedEffect(Unit) {
         scaffoldViewState.value = ScaffoldViewState(
@@ -38,17 +52,38 @@ fun AddProcess(navController: NavController, scaffoldViewState: MutableState<Sca
         )
     }
 
+    LaunchedEffect(addProcessState) {
+        when {
+            addProcessState.isSuccess -> {
+                Toast.makeText(context, "Proses berhasil ditambahkan!", Toast.LENGTH_SHORT).show()
+                navController.popBackStack()
+            }
+
+            addProcessState.errorMessage != null -> {
+                Toast.makeText(context, addProcessState.errorMessage, Toast.LENGTH_SHORT).show()
+                viewModel.clearErrorMessage()
+            }
+        }
+    }
+
     Column(
         verticalArrangement = Arrangement.spacedBy(4.dp),
         modifier = Modifier
             .fillMaxSize()
-            .padding(start = 12.dp, end = 12.dp, bottom = 80.dp)
+            .padding(start = 12.dp, end = 12.dp)
     ) {
-        TextInput(value = name, onInputChanged = { name = it }, label = "Nama")
         TextInput(
-            value = step,
-            onInputChanged = { step = it },
-            label = "Proses ke-"
+            value = addProcessState.name,
+            onInputChanged = { viewModel.onInputChange("name", it) },
+            label = "Nama",
+            errorMessage = addProcessState.errors["name"]
+        )
+        TextInput(
+            value = addProcessState.step,
+            onInputChanged = { viewModel.onInputChange("step", it) },
+            label = "Proses ke-",
+            keyboardType = KeyboardType.Number,
+            errorMessage = addProcessState.errors["step"]
         )
 
         // Save button
@@ -56,8 +91,11 @@ fun AddProcess(navController: NavController, scaffoldViewState: MutableState<Sca
         PrimaryButton(
             text = "Simpan",
             onClick = {
-                navController.popBackStack()
+                keyboardController?.hide()
+                viewModel.addProcess()
             },
+            isLoading = isAdding,
+            enabled = isPrimaryButtonEnabled,
             modifier = Modifier.fillMaxWidth()
         )
     }
