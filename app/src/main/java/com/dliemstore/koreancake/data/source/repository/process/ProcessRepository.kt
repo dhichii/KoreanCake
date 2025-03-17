f8174007a0d3ee11a8bdcf1a754c57ed298d043e
Adhicitta Masran <adhicittamasran@gmail.com>
2025-03-17 16:23:24 +0800

    feat(process): add repository function to add process

--- app/src/main/java/com/dliemstore/koreancake/data/source/repository/process/ProcessRepository.kt
+++ app/src/main/java/com/dliemstore/koreancake/data/source/repository/process/ProcessRepository.kt
@@ -1,6 +1,7 @@
 package com.dliemstore.koreancake.data.source.repository.process
 
 import com.dliemstore.koreancake.data.api.service.ProcessService
+import com.dliemstore.koreancake.data.source.remote.request.process.AddProcessRequest
 import com.dliemstore.koreancake.data.source.remote.request.process.UpdateProcessesStepRequest
 import com.dliemstore.koreancake.util.ApiUtils
 import com.dliemstore.koreancake.util.Resource
@@ -13,6 +14,32 @@ import javax.inject.Inject
 class ProcessRepository @Inject constructor(
     private val processService: ProcessService
 ) {
+    fun addProcess(name: String, step: Int) = flow {
+        emit(Resource.Loading())
+
+        try {
+            val response = processService.add(AddProcessRequest(name, step))
+            if (response.isSuccessful) {
+                emit(Resource.Success(Unit, response.code()))
+            } else {
+                val errorResponse = runCatching { ApiUtils.parseError(response) }.getOrNull()
+                val errorMessage = when (response.code()) {
+                    500 -> "Terjadi kesalahan pada server"
+                    else -> errorResponse?.message ?: "Permintaan tidak valid."
+                }
+
+                emit(Resource.Error(errorMessage, response.code(), errorResponse?.errors))
+            }
+        } catch (e: Exception) {
+            val errorMessage = if (e is IOException) {
+                "Tidak ada koneksi internet."
+            } else {
+                "Terjadi kesalahan yang tidak terduga"
+            }
+            emit(Resource.Error(errorMessage))
+        }
+    }.flowOn(Dispatchers.IO)
+
     fun getProcesses() = flow {
         emit(Resource.Loading())
 
