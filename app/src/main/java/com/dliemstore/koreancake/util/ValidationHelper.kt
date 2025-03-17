c103335e74d3e0c3d23b43a236b21a8577c4f76d
Adhicitta Masran <adhicittamasran@gmail.com>
2025-03-17 17:12:42 +0800

    feat: add validation helper to validate if its a integer and minimal integer

--- app/src/main/java/com/dliemstore/koreancake/util/ValidationHelper.kt
+++ app/src/main/java/com/dliemstore/koreancake/util/ValidationHelper.kt
@@ -16,4 +16,13 @@ object ValidationHelper {
             "$fieldName tidak boleh lebih dari $maxLength karakter."
         } else null
     }
+
+    fun isValidInteger(value: String?, fieldName: String): String? {
+        return if (value != null && value.toIntOrNull() == null) "$fieldName hanya boleh berupa angka." else null
+    }
+
+    fun minInteger(value: String?, minValue: Int, fieldName: String): String? {
+        val number = value?.toIntOrNull()
+        return if (number != null && number < minValue) "$fieldName tidak boleh kurang dari $minValue." else null
+    }
 }
