932676f197d96a6580ed3cfdcdf9689f9c6af1cb
Adhicitta Masran <adhicittamasran@gmail.com>
2025-03-18 17:57:37 +0800

    feat(process): integrate EditProcess UI with viewmodel - handle states when success and error - handle error message in each input field - handle loading state in the save button - save button will be enabled when every input field is filled and the value is not the same its initial value

--- app/src/main/java/com/dliemstore/koreancake/ui/screens/process/EditProcess.kt
+++ app/src/main/java/com/dliemstore/koreancake/ui/screens/process/EditProcess.kt
@@ -1,5 +1,6 @@
 package com.dliemstore.koreancake.ui.screens.process
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -11,30 +12,37 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.dliemstore.koreancake.ui.components.PrimaryButton
 import com.dliemstore.koreancake.ui.components.TextInput
 import com.dliemstore.koreancake.ui.navigation.graphs.ScaffoldViewState
 import com.dliemstore.koreancake.ui.navigation.graphs.TopAppBarItem
 import com.dliemstore.koreancake.ui.navigation.graphs.TopAppBarNavigationIcon
-import com.dliemstore.koreancake.util.getProcessById
+import com.dliemstore.koreancake.ui.viewmodel.process.EditProcessViewModel
 
 @Composable
 fun EditProcess(
     id: String,
     navController: NavController,
-    scaffoldViewState: MutableState<ScaffoldViewState>
+    scaffoldViewState: MutableState<ScaffoldViewState>,
+    viewModel: EditProcessViewModel = hiltViewModel()
 ) {
-    val process = getProcessById(id)
+    val context = LocalContext.current
+    val keyboardController = LocalSoftwareKeyboardController.current
 
-    var name by rememberSaveable { mutableStateOf(process.name) }
-    var step by rememberSaveable { mutableStateOf(process.step.toString()) }
+    val editProcessState by viewModel.editProcessState.collectAsState()
+
+    val isPrimaryButtonEnabled =
+        editProcessState.name.isNotBlank() && editProcessState.step.isNotBlank() &&
+                (editProcessState.name != viewModel.initialName || editProcessState.step != viewModel.initialStep)
+    val isEditing = editProcessState.isLoading
 
     LaunchedEffect(Unit) {
         scaffoldViewState.value = ScaffoldViewState(
@@ -45,17 +53,37 @@ fun EditProcess(
         )
     }
 
+    LaunchedEffect(editProcessState) {
+        when {
+            editProcessState.isSuccess -> {
+                Toast.makeText(context, "Proses berhasil diubah!", Toast.LENGTH_SHORT).show()
+                navController.popBackStack()
+            }
+
+            editProcessState.errorMessage != null -> {
+                Toast.makeText(context, editProcessState.errorMessage, Toast.LENGTH_SHORT).show()
+                viewModel.clearErrorMessage()
+            }
+        }
+    }
+
     Column(
         verticalArrangement = Arrangement.spacedBy(4.dp),
         modifier = Modifier
             .fillMaxSize()
             .padding(start = 12.dp, end = 12.dp, bottom = 80.dp)
     ) {
-        TextInput(value = name, onInputChanged = { name = it }, label = "Nama")
         TextInput(
-            value = step,
-            onInputChanged = { step = it },
-            label = "Proses ke-"
+            value = editProcessState.name,
+            onInputChanged = { viewModel.onInputChange("name", it) },
+            label = "Nama",
+            errorMessage = editProcessState.errors["name"]
+        )
+        TextInput(
+            value = editProcessState.step,
+            onInputChanged = { viewModel.onInputChange("step", it) },
+            label = "Proses ke-",
+            errorMessage = editProcessState.errors["step"]
         )
 
         // Save button
@@ -63,8 +91,11 @@ fun EditProcess(
         PrimaryButton(
             text = "Simpan",
             onClick = {
-                navController.popBackStack()
+                keyboardController?.hide()
+                viewModel.editProcess(id)
             },
+            isLoading = isEditing,
+            enabled = isPrimaryButtonEnabled,
             modifier = Modifier.fillMaxWidth()
         )
     }
