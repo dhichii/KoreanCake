accfc9eace4ad2fd46910a5e0eb1c79b53b1b4e3
Adhicitta Masran <adhicittamasran@gmail.com>
2025-03-18 17:36:17 +0800

    feat(process): add edit process state

---
+++ app/src/main/java/com/dliemstore/koreancake/ui/state/process/EditProcessState.kt
@@ -0,0 +1,11 @@
+package com.dliemstore.koreancake.ui.state.process
+
+data class EditProcessState(
+    val name: String = "",
+    val step: String = "",
+    val errors: Map<String, String?> = emptyMap(),
+    val isLoading: Boolean = false,
+    val isSuccess: Boolean = false,
+    val statusCode: Int? = null,
+    val errorMessage: String? = null
+)
