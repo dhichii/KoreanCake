4875fb0631ce5226fe190a7096996d5f96b9499e
Adhicitta Masran <adhicittamasran@gmail.com>
2025-03-20 23:06:48 +0800

    feat(settings): add viewmodel to fetch user profile

--- app/src/main/java/com/dliemstore/koreancake/ui/viewmodel/settings/SettingsViewModel.kt
+++ app/src/main/java/com/dliemstore/koreancake/ui/viewmodel/settings/SettingsViewModel.kt
@@ -2,7 +2,9 @@ package com.dliemstore.koreancake.ui.viewmodel.settings
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.dliemstore.koreancake.data.source.remote.response.user.UserProfileResponse
 import com.dliemstore.koreancake.data.source.repository.auth.AuthRepository
+import com.dliemstore.koreancake.data.source.repository.user.UserRepository
 import com.dliemstore.koreancake.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
@@ -12,10 +14,23 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SettingsViewModel @Inject constructor(private val authRepository: AuthRepository) :
-    ViewModel() {
+class SettingsViewModel @Inject constructor(
+    private val userRepository: UserRepository,
+    private val authRepository: AuthRepository
+) : ViewModel() {
+    private val _userProfileState =
+        MutableStateFlow<Resource<UserProfileResponse>>(Resource.Loading())
+    val userProfileState: StateFlow<Resource<UserProfileResponse>> = _userProfileState
+
     private val _logoutState = MutableStateFlow<Resource<Unit>?>(null)
     val logoutState: StateFlow<Resource<Unit>?> = _logoutState
+
+    fun fetchUserProfile() {
+        viewModelScope.launch {
+            delay(300)
+            userRepository.getProfile().collect { _userProfileState.value = it }
+        }
+    }
 
     fun logout() {
         _logoutState.value = Resource.Loading()
