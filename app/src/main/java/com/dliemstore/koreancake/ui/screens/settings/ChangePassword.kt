7bdb8d0e1da5b8433a07dd4ee9299a6edb82e16c
Adhicitta Masran <adhicittamasran@gmail.com>
2025-03-21 16:33:36 +0800

    feat(settings): integrate change password UI with viewmodel - handle loading, error, and success state - primary button should enabled when input is not blank and error is null

--- app/src/main/java/com/dliemstore/koreancake/ui/screens/settings/ChangePassword.kt
+++ app/src/main/java/com/dliemstore/koreancake/ui/screens/settings/ChangePassword.kt
@@ -1,47 +1,42 @@
 package com.dliemstore.koreancake.ui.screens.settings
 
-import androidx.compose.foundation.layout.Arrangement
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.dliemstore.koreancake.ui.components.PasswordInput
 import com.dliemstore.koreancake.ui.components.PrimaryButton
 import com.dliemstore.koreancake.ui.navigation.graphs.ScaffoldViewState
 import com.dliemstore.koreancake.ui.navigation.graphs.TopAppBarItem
 import com.dliemstore.koreancake.ui.navigation.graphs.TopAppBarNavigationIcon
+import com.dliemstore.koreancake.ui.viewmodel.settings.ChangePasswordViewModel
 
 @Composable
 fun ChangePassword(
     navController: NavController,
-    scaffoldViewState: MutableState<ScaffoldViewState>
+    scaffoldViewState: MutableState<ScaffoldViewState>,
+    viewModel: ChangePasswordViewModel = hiltViewModel()
 ) {
-    var password by rememberSaveable { mutableStateOf("") }
-    var confirmPassword by rememberSaveable { mutableStateOf("") }
-
-    val hasMinimum: Boolean = password.matches(Regex(".{8,}"))
-    val isMatched: Boolean = confirmPassword == password
-    var isConfirmPasswordChanged by rememberSaveable { mutableStateOf(false) }
+    val context = LocalContext.current
+    val changePasswordState by viewModel.changePasswordState.collectAsState()
+    val isPrimaryButtonEnabled =
+        changePasswordState.oldPassword.isNotBlank() &&
+                changePasswordState.newPassword.isNotBlank() &&
+                changePasswordState.confirmPassword.isNotBlank() &&
+                changePasswordState.errors.isEmpty()
 
     LaunchedEffect(Unit) {
         scaffoldViewState.value = ScaffoldViewState(
@@ -52,38 +47,48 @@ fun ChangePassword(
         )
     }
 
-    Column(modifier = Modifier.padding(horizontal = 12.dp)) {
-        Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
-            PasswordInput(value = password, onInputChanged = { password = it })
-            Row {
-                Icon(
-                    imageVector = if (hasMinimum) Icons.Filled.Check else Icons.Filled.Close,
-                    tint = if (hasMinimum) Color.Green else MaterialTheme.colorScheme.error,
-                    contentDescription = "has minimum"
-                )
-                Spacer(modifier = Modifier.width(4.dp))
-                Text(
-                    text = "Minimal 8 huruf",
-                    color = MaterialTheme.colorScheme.inverseSurface
-                )
+    LaunchedEffect(changePasswordState) {
+        when {
+            changePasswordState.isSuccess -> {
+                Toast.makeText(context, "Password berhasil diubah!", Toast.LENGTH_SHORT).show()
+                navController.popBackStack()
+            }
+
+            changePasswordState.errorMessage != null -> {
+                Toast.makeText(context, changePasswordState.errorMessage, Toast.LENGTH_SHORT).show()
+                viewModel.clearErrorMessage()
             }
         }
-        Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
-            PasswordInput(
-                value = confirmPassword,
-                label = "Konfirmasi Password",
-                onInputChanged = {
-                    confirmPassword = it
-                    if (!isConfirmPasswordChanged) isConfirmPasswordChanged = true
-                },
-            )
-            if (!isMatched && isConfirmPasswordChanged) Text("Konfirmasi password tidak sama")
-        }
+    }
+
+    Column(modifier = Modifier.padding(horizontal = 12.dp)) {
+        PasswordInput(
+            value = changePasswordState.oldPassword,
+            label = "Password Sekarang",
+            onInputChanged = { viewModel.onInputChange("oldPassword", it) },
+            errorMessage = changePasswordState.errors["oldPassword"]
+        )
+        PasswordInput(
+            value = changePasswordState.newPassword,
+            label = "Password Baru",
+            onInputChanged = { viewModel.onInputChange("newPassword", it) },
+            errorMessage = changePasswordState.errors["newPassword"]
+        )
+        PasswordInput(
+            value = changePasswordState.confirmPassword,
+            label = "Konfirmasi Password",
+            onInputChanged = {
+                viewModel.onInputChange("confirmPassword", it)
+            },
+            errorMessage = changePasswordState.errors["confirmPassword"]
+        )
 
         Spacer(modifier = Modifier.height(16.dp))
         PrimaryButton(
             text = "Simpan",
-            onClick = { navController.popBackStack() },
+            enabled = isPrimaryButtonEnabled,
+            isLoading = changePasswordState.isLoading,
+            onClick = { viewModel.changePassword() },
             modifier = Modifier.fillMaxWidth()
         )
     }
