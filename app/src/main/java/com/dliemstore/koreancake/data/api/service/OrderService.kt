bf4e97643cece1010fe3fe13806751f218bf48fe
Adhicitta Masran <adhicittamasran@gmail.com>
2025-03-28 18:00:30 +0800

    feat(order): add API endpoint to add order

--- app/src/main/java/com/dliemstore/koreancake/data/api/service/OrderService.kt
+++ app/src/main/java/com/dliemstore/koreancake/data/api/service/OrderService.kt
@@ -1,19 +1,32 @@
 package com.dliemstore.koreancake.data.api.service
 
 import com.dliemstore.koreancake.data.source.remote.request.order.UpdateOrderProgressRequest
+import com.dliemstore.koreancake.data.source.remote.response.CreatedResponse
 import com.dliemstore.koreancake.data.source.remote.response.PaginationSuccessResponse
 import com.dliemstore.koreancake.data.source.remote.response.SuccessResponse
 import com.dliemstore.koreancake.data.source.remote.response.order.OrderDetailResponse
 import com.dliemstore.koreancake.data.source.remote.response.order.OrdersResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.Multipart
+import retrofit2.http.POST
 import retrofit2.http.PUT
+import retrofit2.http.Part
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface OrderService {
+    @Multipart
+    @POST("orders")
+    suspend fun add(
+        @Part pictures: List<MultipartBody.Part>,
+        @Part("data") data: RequestBody
+    ): Response<SuccessResponse<CreatedResponse<String>>>
+
     @GET("orders")
     suspend fun getAll(
         @Query("page") page: Int,
